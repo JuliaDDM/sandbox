36cdc213b370cbe81572e462c70b2a56c3ef930e
nataf <nataf@ann.jussieu.fr>
2022-06-02 16:11:31 +0200

    up

---
+++ scripts/VersionSansDictionnaire/doperator.jl
@@ -0,0 +1,193 @@
+
+#     POU::DPOU# , en fait c'est plutôt un Shared_vector (cohérent qui en a besoin). En fait, on peut repousser la question POUM à plus tard.
+# la chose principale est de pouvoir coder ∑_i R_i^T R_i
+###
+# vecteur global -> vecteur decompose coherent ( scattered "eclate" mais pas Shared_vector , decomposed DVector ) ->  vecteur decommpose incoherent
+# |                   |
+# |                   |
+# |                   |
+# alpha*u ---------> alpha*Du (a faire)
+# A*u -------------> DA*Du (fait)
+# (u,v) -----------> (Du,Dv)= ∑_i (Ui,Di V_i) (a faire)
+# ASM independant de la partition de l'unite
+# vecteur global <- vecteur partage coherent
+#
+# vecteur partage pas forcement coherent : les rendre cohérent , (cf. RAS)
+# somme compensée pour etre plus stable vis a vis des erreurs d'arrondi  -->
+#       -->  CF MakeCoherent si partition de l'unite non Booleenne ou Gradient conjugue (aussi???) ?
+
+# # Vincent--> pour changer facilement de dictionnaire
+# struct MyDict{K,V,T<:AbstractDict{K,V}} <: AbstractDict{K, V}
+#     dict::T
+# end
+# # commenter une des deux lignes
+# MyDict{K,V}() where {K,V} = MyDict(Dict{K,V}())
+#
+# MyDict{K,V}() where {K,V} = MyDict(ThreadSafeDict{K,V}())
+#
+# # Dict --> MyDict dans le code
+# # pour eviter de redefinir toutes les fcts de Dict
+# Base.parent(d::MyDict) = d.dict
+
+##ou plus simplement, a la typedef C:
+# const MyDict{K,V} = Dict{K,V}
+# const MyDict{K,V} = ThreadSafeDict{K,V}
+# la vraie perte avec les ThreadSafeDict est éventuellement "aux allocations des clès"
+
+#################################################
+#
+#       struct DOperator
+#
+#################################################
+
+# pour effectuer le produit matrice vecteur V_i =  ∑_j R'_i A R_j^T D_j U_j  (V = A U)
+mutable struct DOperator
+    DDomD::DDomain # domaine de départ décomposé
+    DDomA::DDomain # domaine d'arrivée décomposé
+    matvec # collection of local operators Aij = R'_i A R_j^T, i ∈ DDomA, j ∈ DDomD
+    # à "encapsuler" en un produit matrice vecteur avec un DVector qui vit sur DDomD? ??
+end
+
+
+
+"""
+# Arguments
+- 'DDomD'
+- 'A' : a square matrix given by its entries
+"""
+function DOperator(DDomD, A)
+    DA =  Dict{Tuple{Domain,Domain},SparseMatrixCSC{Float64,Int64}}()
+#    DA = Dict{Tuple{Domain,Domain},SparseMatrixCSC{Float64,Int64}}()
+    ThreadsX.foreach(subdomains( DDomD ))  do sdi
+#    for sdi ∈ subdomains(DDomD)
+        ThreadsX.foreach(subdomains( DDomD ))  do sdj
+#        for sdj ∈ subdomains(DDomD)
+            DA[(sdi, sdj)] = A[global_indices(sdi), global_indices(sdj)]
+        end
+    end
+    function shared_mat_vec( x )
+        dom = x.domain
+        res = zeros(dom)
+        y = zeros(dom)
+        #Diboolean pour avoir plus de reproductibilité?
+#        di = Di( dom )
+        di = Diboolean( dom )
+        ThreadsX.foreach(subdomains( dom ))  do sdi
+#        for sdi ∈ subdomains( dom )
+            values( y , sdi ) .= values(di,sdi) .* values(x,sdi)
+        end
+        # boucle parallelisable
+        ThreadsX.foreach(subdomains( res ))  do sdi
+        # for sdi ∈ subdomains( res )
+           values(res , sdi) .= DA[(sdi,sdi)]*values(y , sdi)
+        end
+     # boucle exterieur parallelisable
+     ThreadsX.foreach(subdomains( y ))  do sdi
+        # for sdi ∈ subdomains( y )
+           # boucle sequentiel
+          # ThreadsX.foreach(subdomains( y ))  do sdj essayé pour voir et effectivement non parallelisable sous cette forme.
+           for sdj ∈ subdomains( y )# ou plus efficace et plus clair , passer par les cles avec premier element fixe et deuxieme sdj
+              if ( haskey(DA , (sdi,sdj) ) &&  !( sdi == sdj))
+                 values(res , sdi) .+= DA[(sdi,sdj)]*values(y , sdj)
+              end
+           end
+        end
+        return MakeCoherent(res)
+     end
+     return DOperator( DDomD , DDomD , shared_mat_vec )
+end
+
+import Base.*
+
+function (*)( A::DOperator , v)
+    return A.matvec( v)
+end
+
+
+"""
+# Arguments
+- 'DDomD'
+- 'A' : a square matrix given by its entries
+"""
+function DOperatorSequential(DDomD, A)
+    DA = Dict{Tuple{Domain,Domain},SparseMatrixCSC{Float64,Int64}}()
+    for sdi ∈ subdomains(DDomD)
+        for sdj ∈ subdomains(DDomD)
+            DA[(sdi, sdj)] = A[global_indices(sdi), global_indices(sdj)]
+        end
+    end
+    function shared_mat_vec( x )
+        dom = x.domain
+        res = zeros(dom)
+        y = zeros(dom)
+        #Diboolean pour avoir plus de reproductibilité?
+#        di = Di( dom )
+        di = Diboolean( dom )
+        for sdi ∈ subdomains( dom )
+            values( y , sdi ) .= values(di,sdi) .* values(x,sdi)
+        end
+        # boucle parallelisable
+        for sdi ∈ subdomains( res )
+           values(res , sdi) .= DA[(sdi,sdi)]*values(y , sdi)
+        end
+     # boucle exterieur parallelisable
+        for sdi ∈ subdomains( y )
+           # boucle sequentiel
+           for sdj ∈ subdomains( y )# ou plus efficace et plus clair , passer par les cles avec premier element fixe et deuxieme sdj
+              if ( haskey(DA , (sdi,sdj) ) &&  !( sdi == sdj))
+                 values(res , sdi) .+= DA[(sdi,sdj)]*values(y , sdj)
+              end
+           end
+        end
+        return MakeCoherent(res)
+     end
+     return DOperator( DDomD , DDomD , shared_mat_vec )
+end
+
+
+
+#################################################
+#
+#       struct DOperatorBlockJacobi
+#
+#################################################
+
+struct DOperatorBlockJacobi
+    DDomD::DDomain # domaine de départ et d'arrivée décomposé
+    DDomA::DDomain # domaine de départ et d'arrivée décomposé
+    matvec # collection of local solvers
+end
+
+"""
+DOperatorBlockJacobi(DDomD, A)
+
+Returns direct local solvers for the Dirichlet matrices of a global matrix A
+# Arguments
+- 'DDomD'
+- 'A' : a square matrix given by its entries
+"""
+function DOperatorBlockJacobi(DDomD, A)
+    # DA_lu = ThreadSafeDict()
+    # ThreadsX.foreach(subdomains( DDomD ))  do sdi
+    #     #        for sdi ∈ subdomains(DDomD)
+    #     DA_lu[sdi] = factorize(A[global_indices(sdi), global_indices(sdi)]  )
+    # end
+
+    DA_lu = Dict()
+    ThreadsX.foreach(subdomains( DDomD ))  do sdi
+#    for sdi ∈ subdomains(DDomD)
+        DA_lu[sdi] = factorize(A[global_indices(sdi), global_indices(sdi)]  )
+    end
+
+    function shared_mat_vec( x )
+        dom = x.domain
+        res = DVector( dom , 0. )
+        # boucle parallelisable , Di a ajouter somewhere
+        ThreadsX.foreach( subdomains( res ))  do sdi
+#        for sdi ∈ subdomains( res )
+            values(res , sdi) .= DA_lu[sdi]\ values(x , sdi)
+        end
+        return res
+    end
+    return DOperatorBlockJacobi( DDomD , DDomD , shared_mat_vec )
+end
---
+++ scripts/VersionSansDictionnaire/dvector.jl
@@ -0,0 +1,293 @@
+#################################################
+#
+#       struct DVector
+#
+#################################################
+mutable struct DVector{T}
+    domain::DDomain
+    data::Vector{Pair{Domain,Vector{T}}}# cf. collect(Dict{Domain,Vector{T}}())
+    # + , - , a* , .* , similar etc ... si on peut automatiquement hériter de ce qui vient de vecteur, on a gagné voir comment faire en Julia
+    # boucles sur eval ??
+    # ce qui est lié à l'aspect cohérent : prodscal et donc demande une partition de l'unite qulle quelle soit
+    # relation avec les vecteurs "habituels" : DVector2Vector
+end
+
+function subdomains(DVect::DVector)
+    return subdomains(DVect.domain)
+end
+
+import Base.values
+function values(DVect::DVector, sd::Domain)
+    pos =  findfirst( x -> isequal(x.first,sd) , DVect.data)
+    return DVect.data[pos].second
+end
+
+
+function DVector(ddomain::DDomain, initial_value::T) where {T<:Number}
+    datatype = typeof(initial_value)
+    data_res = Vector{Pair{Domain,Vector{datatype}}}(undef,length(ddomain))
+    ThreadsX.foreach( enumerate(ddomain.subdomains) )  do ( i , sdi )
+        data_res[i] = ( sdi => fill( initial_value , length(sdi) ) )
+    end
+    return DVector(ddomain, data_res)
+end
+
+
+"""
+basic printing of a decomposed vector
+"""
+function vuesur(U::DVector)
+    for ( i , sdvec) ∈ U.data
+        println(i,"   ",sdvec)
+    end
+end
+
+
+"""
+returns a decomposed vector R_i ∑_j R_j^T U_j
+"""
+function Update(DVec::DVector)
+    res = DVector(DVec.domain, 0.0)
+    ThreadsX.foreach( zip(res.data,DVec.data) )  do ( (sdres , vecres) , (sdsrc , vecsrc) )
+        vecres .= vecsrc
+    end
+    ThreadsX.foreach( enumerate(DVec.domain.subdomains) )  do ( i , sdi )
+        for sdvois ∈ DVec.domain.overlaps[i]
+            res.data[i].second[sdvois.second[1]] .+= DVec.data[ sdvois.first ].second[sdvois.second[2]]
+        end
+    end
+    return res
+end
+
+
+
+import Base.* , Base.fill!
+function *( a::T , x::DVector{T} )  where {T<:Number}
+    res = DVector(x.domain, zero(T))
+    ThreadsX.foreach( zip( res.data , x.data ) )  do ( (sdres , vecres) , (sdsrc , vecsrc) )
+        vecres .= a * vecsrc
+    end
+    return res
+end
+
+function dot_op(x::DVector{T}, y::DVector{T}, dot_op) where {T<:Number}
+    if !(x.domain == y.domain)
+        error("Domains of both decomposed vectors must be the same")
+    end
+    res = DVector( x.domain , zero(T) )
+    ThreadsX.foreach( zip( res.data , x.data , y.data ) )  do ( (sdres , vecres) , (sdx ,vecx) , (sdy , vecy) )
+        vecres .= dot_op( vecx , vecy )
+    end
+    return res
+end
+
+
+"""
+Dibooleannaive( domain )
+
+returns a decomposed vector that corresponds to a Boolean partition of unity function
+# Arguments
+- 'domain' : the support domain
+"""
+function Dibooleannaive(ddomain::DDomain)
+    res = DVector(ddomain, 1.0)
+    ThreadsX.foreach( enumerate(subdomains(res))  ) do ( i , sdi )
+         for jvois ∈ intersect( collect(i+1:length(ddomain)) , keys(ddomain.overlaps[i])  )
+           res.data[jvois].second[ddomain.overlaps[i][jvois][2]] .= 0.0
+         end
+    end
+    return res
+end
+
+# premiere version
+function my_memoize(f)
+    mem = Dict()
+    function memf(x)
+        if x ∈ keys(mem)
+#            println("I know you")
+            return mem[x]
+        end
+        mem[x] = f(x)
+        return mem[x]
+    end
+    return memf
+end
+
+Diboolean = my_memoize(Dibooleannaive)
+
+
+"""
+MakeCoherent( dvector )
+
+Returns a coherent decomposed vector
+Using a Boolean partition of unity ensures that the result is roundoff error free and execution order independent
+R_i ∑_j R_j^T D_j U_j
+# Argument
+- dvector : a decomposed vector
+"""
+function MakeCoherent(DVect::DVector)
+    #Diboolean ensures that the result is roundoff error free
+#    return Update(dot_op(Diboolean(DVect.domain), DVect, (.*)))
+    Di = Diboolean(DVect.domain)
+    return MakeCoherent(DVect,Di)
+end
+
+
+"""
+MakeCoherent( dvector , Di )
+
+Returns a coherent decomposed vector
+R_i ∑_j R_j^T D_j U_j
+# Argument
+- dvector : a decomposed vector
+- Di      : a partition of unity vector
+"""
+function MakeCoherent(DVect::DVector , Di::DVector)
+    if !(DVect.domain == Di.domain)
+        error("Domains of both decomposed vectors must be the same")
+    end
+    return Update(dot_op(Di, DVect, (.*)))
+end
+
+# """
+# MakeCoherent!( dvector )
+#
+# Makes a decomposed vector coherent BOGUE
+# Using a Boolean partition of unity ensures that the result is roundoff error free and execution order independent
+# # Argument
+# - dvector : a decomposed vector
+# """
+# function MakeCoherent!(DVect::DVector)
+#     #Diboolean ensures that the result is roundoff error free
+#     tmp=copy(DVect)
+#     Dvect = copy(MakeCoherent(tmp))
+# end
+
+
+import Base.ones, Base.zeros , Base.rand
+for sym in [ :ones , :zeros , :rand ]
+    @eval function $(Symbol(string(sym)))(ddomain::DDomain)
+        # Float64 should be inferred automatically
+        data_res = Vector{Pair{Domain,Vector{Float64}}}(undef,length(ddomain));
+        ThreadsX.foreach( enumerate(ddomain.subdomains) )  do ( i , sdi )
+            data_res[i] = ( sdi => $sym( length(sdi) ) );
+        end
+    return MakeCoherent(DVector(ddomain, data_res))
+    end
+end
+
+"""
+DVector( domain , vecsrc )
+
+Returns a decomposed vector built from a vector
+# Arguments
+- 'domain' : the decomposed domain
+- 'vecsrc' : the classical vector
+"""
+function DVector(ddomain::DDomain, Usrc::Vector{T}) where {T<:Number}
+    if !(length(ddomain.up) == length(Usrc))
+        error("Lengthes of decomposed domain and vector must match: $(length(ddomain.up)) is not $(length(Usrc)) ")
+    end
+    res = DVector( ddomain, zero(T) )
+    #what if Usrc is already a decomposed vector??
+    ThreadsX.foreach( enumerate(ddomain.subdomains) )  do  ( i , sdi )
+    #    values(res, sd) .= Usrc[global_indices(sd)]
+        res.data[i].second .= Usrc[global_indices(sdi)]
+    end
+    return res
+end
+
+
+"""
+Dimultiplicity( domain )
+
+returns a decomposed vector that corresponds to a multiplicity based partition of unity function
+# Arguments
+- 'domain' : the support domain
+"""
+function Dimultiplicity(domain::DDomain)
+    tmp = DVector(domain, 1.0)
+    multiplicity = Update(tmp)
+    res = dot_op(tmp, multiplicity, (./))
+    return res
+end
+
+
+function noncoherentrandDVector(ddomain::DDomain)
+    data_res = Vector{Pair{Domain,Vector{Float64}}}(undef,length(ddomain))
+    ThreadsX.foreach( enumerate(ddomain.subdomains) )  do ( i , sdi )
+        data_res[i] = ( sdi => rand(length(sdi)))
+    end
+    return DVector(ddomain, data_res)
+end
+
+
+
+import  Base.similar
+# first try of Metaprogramming
+for sym in [ :similar   ]
+    @eval function $(Symbol(string(sym)))(a::DVector)
+        res = DVector(a.domain, 0.0)
+        ThreadsX.foreach( enumerate(subdomains(res)) )  do ( i , sdi )
+            res.data[i].second .=  $sym( a.data[i].second )
+        end
+        return res
+    end
+end
+
+import Base.copy
+function copy(DVec::DVector)
+    res = DVector(DVec.domain, 0.0)
+    ThreadsX.foreach( enumerate(subdomains( DVec )) )  do ( i , sdi )
+    res.data[i].second .=  DVec.data[i].second
+    end
+    return res
+end
+
+import LinearAlgebra.dot
+"""
+dot( U , V )
+
+returns the scalar product of two coherent decomposed vectors
+ ∑_i ( U , Diboolean(V) )
+# Arguments
+- 'U and V' : two decomposed vectors
+"""
+function dot(Da::DVector,Db::DVector)
+    tmp = dot_op( Diboolean(Db.domain)  , Db , (.*) )
+# time and allocation could be saved by not creating tmp but making a .* method with three vectors:
+    res = ThreadsX.sum(   dot( sdveca.second ,  sdvectmp.second ) for ( sdveca , sdvectmp ) ∈ zip(Da.data,tmp.data) )
+    return res
+end
+
+
+function fill!(x::DVector{T} , a::T )  where {T<:Number}
+    ThreadsX.foreach( x.data )  do sdvec
+#    for sd ∈ subdomains(res)
+        fill!( sdvec.second , a)
+    end
+end
+
+
+import Base.+
+function +( x::DVector , y::DVector )
+    return dot_op( x , y , +)
+end
+
+# DV1 .* DV2 , iterable venant d'un abstractvector , risque de perdre le //
+# Vincent --> ou surcharger broadcast
+
+
+
+function DVector2Vector(DVect::DVector{T}) where {T<:Number}
+    Dres = MakeCoherent(DVect)
+    ddomain = DVect.domain
+    res = zeros( T , length(ddomain.up) )
+    #peu compatible avec une parallelisation
+    #il faudrait differencier selon que Diboolean est zero ou non
+    # si je mets ThreadSafeDict ici (c.a.d.????), j'ai une erreur à l'execution. => strange ??
+    for (sd, val) ∈ Dres.data
+        res[global_indices(sd)] .= val
+    end
+    return res
+end
